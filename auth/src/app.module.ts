313529318bdf537573f0e2a0d2703880f4eb1384
dongnd <dongnd@smartosc.com>
2023-03-13 16:45:11 +0700

    config env file for each evironment

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -28,18 +28,46 @@ import { UserModule } from './user/user.module';
     }),
     // DatabaseModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    ClientsModule.register([
+    // ClientsModule.register([
+    //   {
+    //     name: 'AUTH_MICROSERVICE',
+    //     transport: Transport.KAFKA,
+    //     options: {
+    //       client: {
+    //         clientId: 'auth',
+    //         brokers: ['kafka:9092'],
+    //       },
+    //       consumer: {
+    //         groupId: 'auth-consumer',
+    //       },
+    //     },
+    //   },
+    // ]),
+    ClientsModule.registerAsync([
       {
         name: 'AUTH_MICROSERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: 'auth',
-            brokers: ['kafka:9092'],
-          },
-          consumer: {
-            groupId: 'auth-consumer',
-          },
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          console.log('config', configService.get('BROKER_KAFKA'));
+          console.log('process', process.env.BROKER_KAFKA);
+          console.log('env', process.env.NODE_ENV);
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: 'auth',
+                brokers: [
+                  // configService.get('BROKER_KAFKA') ?? process.env.BROKER_KAFKA,
+                  'kafka:9092',
+                ],
+                // ssl: true,
+              },
+              consumer: {
+                groupId: 'auth-consumer',
+              },
+            },
+          };
         },
       },
     ]),
